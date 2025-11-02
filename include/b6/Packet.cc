1f28e5e16a17cf7186380821204fe0b859fd4ea4
angrybat <jasonmoore95@hotmail.com>
2025-11-02 17:34:27 +0000

    Fixed warning after including Packet.cc

--- include/b6/Packet.cc
+++ include/b6/Packet.cc
@@ -21,7 +21,7 @@
 namespace b6 {
   std::string Packet::readString(size_t len) {
     std::string ret;
-    for (int i = 0; i < len; i++) {
+    for (size_t i = 0; i < len; i++) {
       ret += (char)readU8();
     }
     return ret;
