370989e6c51762ba5f3f6a994e6c26679cfaefd5
angrybat <jasonmoore95@hotmail.com>
2025-11-02 18:54:53 +0000

    Added logs when exceptions are thrown to help debug

--- src/DeviceBridge.cc
+++ src/DeviceBridge.cc
@@ -1,6 +1,7 @@
 #include "b6/DeviceBridge.h"
 #include <exception>
 #include <memory>
+#include <iostream>
 
 DeviceHandle::DeviceHandle(std::unique_ptr<b6::Device> d) : dev(std::move(d)) {}
 
@@ -8,9 +9,11 @@ std::unique_ptr<DeviceHandle> DeviceHandle::new_device() {
   try {
     auto d = std::make_unique<b6::Device>();
     return std::make_unique<DeviceHandle>(std::move(d));
-  } catch (const std::exception&) {
+  } catch (const std::exception& e) {
+    std::cerr << "Could not create device: " << e.what() << std::endl;
     return nullptr;
   } catch (...) {
+    std::cerr << "Could not create device due to unknown exception" << std::endl;
     return nullptr;
   }
 }
