4b497019151ab0d7f4db00dda0eac84509b33f96
angrybat <jasonmoore95@hotmail.com>
2025-11-02 16:40:50 +0000

    Added devcontainer.json and post create command script

---
+++ .devcontainer/post_create_command.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Install Dependencies
+sudo apt-get update -y
+sudo apt-get install -y build-essential cmake clang++ libclang libusb-1.0-0-dev
+
+# Install Dagger CLI
+curl -fsSL https://dl.dagger.io/dagger/install.sh | DAGGER_VERSION=0.19.2 BIN_DIR=$HOME/.local/bin sh
+mkdir -p /home/vscode/.local/share/bash-completion/completions
+dagger completion bash > /home/vscode/.local/share/bash-completion/completions/dagger
