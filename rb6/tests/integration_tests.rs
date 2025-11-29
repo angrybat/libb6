e17dc8adca1bdabd6773ea7805332134110036ca
angrybat <jasonmoore95@hotmail.com>
2025-11-29 21:08:02 +0000

    Started adding integration tests for the new_device method

---
+++ rb6/tests/integration_tests.rs
@@ -0,0 +1,41 @@
+mod new_device {
+    use rb6::new_device;
+    use mockb6;
+
+    #[test]
+    fn returns_error_when_libusb_init_does_not_return_0() {
+        {
+            let mut mock = mockb6::MOCK.lock().unwrap();
+            mock.init_return = -1;
+        }
+    
+        let mut device_handle_result = new_device();
+    
+        if let Some(result) = device_handle_result.as_mut() {
+            assert!(result.get_error() == "Could not create device: libusb err: -1");
+            assert!(result.get_device_handle().is_null());
+        } 
+        else {
+            panic!("Expected DeviceHandleResult, got None");
+        }
+    }
+    
+    #[test]
+    fn returns_error_when_libusb_open_with_vid_pid_returns_nullptr() {
+        {
+            let mut mock = mockb6::MOCK.lock().unwrap();
+            mock.init_return = 0;
+            mock.open_return = 0;
+        }
+    
+        let mut device_handle_result = new_device();
+    
+        if let Some(result) = device_handle_result.as_mut() {
+            assert!(result.get_error() == "Could not create device: cannot find/open b6 device");
+            assert!(result.get_device_handle().is_null());
+        } 
+        else {
+            panic!("Expected DeviceHandleResult, got None");
+        }
+    }
+}
