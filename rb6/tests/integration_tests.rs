46ae3ec7923dbaec9105ebcc133c5ee4e12b91eb
angrybat <jasonmoore95@hotmail.com>
2025-11-30 15:25:13 +0000

    Added more tests to check faliure situations with libusb

--- rb6/tests/integration_tests.rs
+++ rb6/tests/integration_tests.rs
@@ -38,4 +38,46 @@ mod new_device {
             panic!("Expected DeviceHandleResult, got None");
         }
     }
+
+    #[test]
+    fn returns_error_when_libusb_cant_detach_kernel_driver() {
+        {
+            let mut mock = mockb6::MOCK.lock().unwrap();
+            mock.init_return = 0;
+            mock.open_return = 0x1;
+            mock.detach_return = -5;
+            mock.kernel_driver_active = 1;
+        }
+    
+        let mut device_handle_result = new_device();
+    
+        if let Some(result) = device_handle_result.as_mut() {
+            assert!(result.get_error() == "Could not create device: cannot detach kernel driver, err: -5");
+            assert!(result.get_device_handle().is_null());
+        } 
+        else {
+            panic!("Expected DeviceHandleResult, got None");
+        }
+    }
+
+    #[test]
+    fn returns_error_when_libusb_cant_claim_interface() {
+        {
+            let mut mock = mockb6::MOCK.lock().unwrap();
+            mock.init_return = 0;
+            mock.open_return = 0x1;
+            mock.kernel_driver_active = 0;
+            mock.claim_return = -3;
+        }
+    
+        let mut device_handle_result = new_device();
+    
+        if let Some(result) = device_handle_result.as_mut() {
+            assert!(result.get_error() == "Could not create device: cannot claim interface 0, err: -3");
+            assert!(result.get_device_handle().is_null());
+        } 
+        else {
+            panic!("Expected DeviceHandleResult, got None");
+        }
+    }
 }
